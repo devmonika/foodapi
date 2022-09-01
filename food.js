cf9648bb35148801e09950556ccd21f0b8fe5d93
devmonika <devmonikamomi@gmail.com>
2022-09-01 09:27:17 +0600

    find another problem

--- food.js
+++ food.js
@@ -15,7 +15,7 @@ const displayFood = meals => {
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-        <div class="card">
+        <div onclick="getMealDetails(${meal.idMeal})" class="card">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -33,4 +33,33 @@ const searchFood = () => {
     getFood(searchText);
     searchfld.value = '';
 }
-// getFood("a");
+
+
+const getMealDetails = (idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+}
+
+
+const displayMealDetails = () => {
+    const detailesContainer = document.getElementById('detailes-container');
+    detailesContainer.innerHTML = '';
+    const detailesDiv = document.createElement('div');
+    detailesDiv.classList.add('card');
+    detailesDiv.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${meal.strMeal}</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+`;
+    detailesContainer.appendChild(detailesDiv);
+}
+
+
+
+getFood();
